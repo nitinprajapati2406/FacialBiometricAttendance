b5f7f1dce5edbf81795e4de4196a3a4b68193878
nitinprajapati2406 <138984811+nitinprajapati2406@users.noreply.github.com>
2023-09-20 23:35:23 +0530

    Update admin.py

--- FacialBiometricAttendance/AttendanceApp/admin.py
+++ FacialBiometricAttendance/AttendanceApp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from . models import FaceId
+from . models import FaceId, Records
 # Register your models here.
 admin.site.register(FaceId)
+admin.site.register(Records)
